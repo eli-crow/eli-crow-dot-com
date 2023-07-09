292789f880545a99e9b741d2da4df9b6473f9db2
eli-crow <emcrow@sep.com>
2023-07-09 10:22:04 -0400

    Update min heights on TheShowoffPage

--- components/TheShowoffPage.vue
+++ components/TheShowoffPage.vue
@@ -15,14 +15,20 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="bg-gray-50 self-stretch flex flex-col relative flex-1 md:fixed md:inset-0 md:flex-row md:gap-8 md:p-8">
+    <div class="
+    bg-gray-50 self-stretch flex flex-col relative flex-1
+    md:fixed md:inset-0 md:flex-row md:gap-8 md:p-8
+    ">
         <!-- 
         HACK: translate3d: maddening iOS issue where z-index unpredictably doesn't work unless you got here from the homepage. 
         I think it is because of the way ios treats 3d canvases in the stacking order. Basically, they act as if they have 3d transforms,
         so z-index is not respected, and you must use the z dimension to achieve the desired effect.
-        -->
+    -->
         <header :style="{ transform: 'translate3d(0,0,10px)' }"
-                class="flex flex-col flex-grow relative z-10 w-full p-8 coarse:pt-4 rounded-t-3xl bg-white dark:bg-gray-100 md:p-4 md:max-w-xs md:!bg-transparent md:dark:!bg-transparent md:rounded-none md:flex-auto md:self-center">
+                class="
+                flex flex-col flex-grow relative z-10 w-full p-8 coarse:pt-4 rounded-t-3xl bg-white dark:bg-gray-100 min-h-[calc(100vh-24rem)]
+                md:p-4 md:max-w-xs md:!bg-transparent md:dark:!bg-transparent md:rounded-none md:flex-auto md:self-center md:min-h-0
+                ">
             <!-- draggable indicator -->
             <div class="fine:hidden md:hidden h-[0.3333rem] mb-2 w-12 self-center bg-gray-100 dark:bg-gray-200 rounded-full" />
 
